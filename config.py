4d2192b4cb33c4a10937a9ab8f95a9c803745adc
Jeetendra800 <jeetendra2949@gmail.com>
2025-12-13 22:18:17 +0530

    Set default values for API and configuration variables
    Updated environment variable defaults with specific values.

--- config.py
+++ config.py
@@ -13,14 +13,14 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
+API_ID = int(getenv("API_ID", "20288395"))
+API_HASH = getenv("API_HASH", "1245317c7706166809189b4a7918b79c")
+BOT_TOKEN = getenv("BOT_TOKEN", "8538134836:AAEuDJEfchE-QNslpGeMD7avohn7V8ZtxQs")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "8429266518").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://jeetendra2949_db_user:4FE5y1WlDhZSGV1Y@cluster0.glaqca4.mongodb.net/?appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1003320690267")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003357360615"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "100"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
 AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
